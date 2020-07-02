e839129b891d69d12d42d15b4704821c48ec0688
MarbinSpectrum <9507ym@naver.com>
2020-07-02 21:47:29 +0900

    D

---
+++ HearthStone/Assets/Graphics/Shaders/RenderCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCamera : MonoBehaviour
+{
+
+    private RenderTexture texture;
+    [Header("Cameras")]
+    public Camera camera;
+
+    public void Awake()
+    {
+        texture = new RenderTexture(1920, 1080, 0);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        RenderTexture tempRenderTexture = RenderTexture.GetTemporary(source.width, source.height);
+
+        Graphics.Blit(texture, tempRenderTexture);
+        camera.targetTexture = texture;
+        camera.Render();
+
+
+     
+    }
+}
