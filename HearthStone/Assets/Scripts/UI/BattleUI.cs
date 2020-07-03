6c3eb6b0a26a1ca189a797b2c88da8073f9c5c01
MarbinSpectrum <9507ym@naver.com>
2020-07-03 21:43:02 +0900

    D

--- HearthStone/Assets/Scripts/UI/BattleUI.cs
+++ HearthStone/Assets/Scripts/UI/BattleUI.cs
@@ -7,13 +7,17 @@ public class BattleUI : MonoBehaviour
     public ShowPlayerText playerText;
     public ShowPlayerText enermyText;
 
+    public Mulligan mulligan;
+
     private void Start()
     {
         StartCoroutine(ShowEnermyText(4, "내가 대자연을 수호하겠다!"));
+        StartCoroutine(ShowMulligan(4, 1));
         StartCoroutine(ShowPlayerText(6, "자연은 반드시 보호해야한다!"));
+
     }
 
-    #region[대사보여주기]
+    #region[등장대사]
     private IEnumerator ShowPlayerText(float waitTime,string s)
     {
         yield return new WaitForSeconds(waitTime);
@@ -26,6 +30,14 @@ public class BattleUI : MonoBehaviour
         yield return new WaitForSeconds(waitTime);
         enermyText.show = true;
         enermyText.text = s;
+    }
+    #endregion
+
+    #region[멀리건]
+    private IEnumerator ShowMulligan(float waitTime,int n)
+    {
+        yield return new WaitForSeconds(waitTime);
+        mulligan.animator.SetInteger("State", n);
     }
     #endregion
 
---
+++ HearthStone/Assets/Scripts/UI/멀리건/Mulligan.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mulligan : MonoBehaviour
+{
+    public CardView[] cardView;
+
+    public Animator animator;
+}
