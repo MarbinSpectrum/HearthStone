62d1df2ca43babdf8ec11498c724c1f34b1708b9
MarbinSpectrum <9507ym@naver.com>
2022-10-08 21:28:28 +0900

    쉐이더 코드 수정

---
+++ HearthStone/Assets/Graphics/Sprites/Minions/SetGrayPower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetGrayPower : MonoBehaviour
+{
+    [SerializeField] private Renderer renderer;
+    [SerializeField] [Range(0, 1)] private float _GrayPower;
+
+    private MaterialPropertyBlock mpb;
+
+    private void Update()
+    {
+        if(renderer == null)
+            return;
+
+        if (mpb == null)
+        {
+            mpb = new MaterialPropertyBlock();
+        }
+
+        renderer.GetPropertyBlock(mpb, 0);
+        mpb.SetFloat("_GrayPower", _GrayPower);
+        renderer.SetPropertyBlock(mpb, 0);
+    }
+}
